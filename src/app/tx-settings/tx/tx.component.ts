56ea51f45a739072b49fa9fe1d2012e5680b789e
Gasparini16 <lagowskipiotr16@gmail.com>
2018-09-22 12:55:38 +0200

    Add Formular for tx settings

--- src/app/tx-settings/tx/tx.component.ts
+++ src/app/tx-settings/tx/tx.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup} from '@angular/forms';
 
 @Component({
   selector: 'app-tx',
@@ -6,10 +7,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./tx.component.less']
 })
 export class TxComponent implements OnInit {
+  txForm: FormGroup;
 
-  constructor() { }
+  constructor(
+    private formBuilder: FormBuilder
+  ) { }
 
   ngOnInit() {
+    this.txForm = this.buildTxForm();
+  }
+
+  buildTxForm() {
+    return this.formBuilder.group({
+      txPower: '',
+      frequency: '',
+      antenna: '',
+      propagationModel: ''
+    });
   }
 
 }
